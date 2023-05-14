fa2b18a720732c31ca6670336f98e1180d520674
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-05-14 14:15:07 +0000

    misc. config

--- lua/autopairs_config.lua
+++ lua/autopairs_config.lua
@@ -1,1 +1,13 @@
-require("nvim-autopairs").setup {}
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
+
+npairs.setup {}
+
+npairs.add_rules({
+    Rule("$", "$", {"tex", "latex"})
+    -- move right when repeating $
+    :with_move(cond.done())
+    -- disable adding a newline when you press <cr>
+    :with_cr(cond.none()),
+})
