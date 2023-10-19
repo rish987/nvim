6aa7a837f435c7c83ee7490cdd1ac8c2fb739779
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-10-19 18:33:00 +0200

    implement main multifun functionality

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -56,7 +56,110 @@ end, 3)
 --     return false
 --   end
 -- end
---
+
+local multifun = function(_actions)
+  return function()
+    local actions
+    if type(_actions) == "function" then
+      actions = _actions()
+    else
+      actions = _actions
+    end
+
+    local conflicting_actions = {}
+    for _, action in ipairs(actions) do
+      if action[1] then -- is a list of equal-priority actions
+        for _, _action in ipairs(action) do
+          if _action.cond() then -- do the first possible action
+            table.insert(conflicting_actions, _action)
+            break
+          end
+        end
+      elseif action.cond() then
+        table.insert(conflicting_actions, action)
+      end
+    end
+
+    -- for _, action in ipairs(conflicting_actions) do
+    --   print(action.desc)
+    -- end
+
+    for i, action in ipairs(conflicting_actions) do
+      if i == #conflicting_actions then
+        action.cb()
+        return
+      end
+
+      -- TODO show extmark hint regarding current conflicts and resolution order
+
+      local key
+      local timed_out = timeout(function (cb) key = vim.fn.getcharstr() cb() end, 1000) -- wait for another tabkey
+      if timed_out then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true))
+        action.cb()
+        return
+      elseif key ~= vim.api.nvim_replace_termcodes("<Tab>", true, true, true) then
+        action.cb()
+        vim.fn.feedkeys(key) -- re-feed the original key if not tab
+        return
+      end
+    end
+  end
+end
+
+local tab_actions = function()
+  local luasnip = require('luasnip')
+  local cmp = require("cmp")
+
+  local col = vim.fn.col('.')
+  local line = vim.fn.getline('.')
+
+  print(col, #line)
+  return {
+    {cb = luasnip.expand, cond = luasnip.expandable, desc = "luasnip expand"},
+    {cb = function() luasnip.jump(1) end, cond = function() return luasnip.jumpable(1) end, desc = "luasnip jump" },
+    {
+      cb = function()
+              cmp.mapping.confirm({ select = true })(function () end)
+           end,
+      cond = function() return cmp.core.view:visible() end,
+      desc = "confirm first completion"
+    },
+    {
+      {
+        cb = function()
+          local next_delim = check_next_delim(line, col)
+          while next_delim do
+            for _ = 1, vim.fn.strdisplaywidth(next_delim) do
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, true, true))
+            end
+
+            col = col + #next_delim
+
+            next_delim = col <= #line and check_next_delim(line, col)
+          end
+        end,
+        cond = function() return check_next_delim(line, col) end,
+        desc = "jump over ending pairs"
+      },
+      {
+        cb = function()
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-o>$", true, true, true))
+        end,
+        cond = function() return col ~= #line + 1 end,
+        desc = "jump to end of line"
+      },
+      {
+        cb = function()
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true))
+        end,
+        cond = function() return col == #line + 1 end,
+        desc = "new line"
+      },
+    },
+  }
+end
+
 local tab_complete = function()
   local luasnip = require('luasnip')
   local cmp = require("cmp")
@@ -106,6 +209,7 @@ local tab_complete = function()
 
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true))
 end
+
 return {
   "rafamadriz/friendly-snippets", -- collection of my_useful snippets
   {
@@ -114,7 +218,7 @@ return {
     build = "make install_jsregexp",
     init = function ()
       -- vim.keymap.set("i", "<C-u>", function() if luasnip then print(luasnip.jumpable(-1)) luasnip.jump(-1) end end, {})
-      vim.keymap.set("i", "<Tab>", require'plenary.async'.void(tab_complete))
+      vim.keymap.set("i", "<Tab>", require'plenary.async'.void(multifun(tab_actions)))
       vim.keymap.set("i", "<C-j>", function() require"luasnip".jump(1) end, {})
 
       vim.api.nvim_create_autocmd('ModeChanged', {
